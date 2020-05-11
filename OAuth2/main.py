83d97281da4dda253ce264d4dfd5a4afe93c0c73
Jeff <dickinsj@oregonstate.edu>
2020-05-11 12:28:53 -0500

    removed direct client id and client secret, all working with comments

--- OAuth2/main.py
+++ OAuth2/main.py
@@ -1,19 +1,40 @@
 import json
 import flask
 import requests
-import oauthSettings as OA
+import rand_code as rc
+import os
 
-app = flask.Flask(__name__)
+app = flask.Flask(__name__, static_url_path='', static_folder='web/static')
 
-#import uuid
-app.secret_key = str(OA.generate_random_code(20))
+#gcp gets angry if this is not in place
+app.secret_key = str(rc.generate_random_code(20))
 
-CLIENT_ID = "489568071894-209q40tq61bg69s3efg1nr9jho98fn7q.apps.googleusercontent.com"
-CLIENT_SECRET = "vI4P_eVHQ8zPuZRthsM0V8GR"
+#get client_secret file from directory and assign values
+client_secret_path = os.path.dirname(os.path.abspath(__file__)) + "/creds/client_secret.json"
+CLIENT_SECRET_FILE = json.load(open(client_secret_path))
+CLIENT_ID = CLIENT_SECRET_FILE['web']['client_id']
+CLIENT_SECRET = CLIENT_SECRET_FILE['web']["client_secret"]
 SCOPE = 'email profile'
 REDIRECT_URI = 'https://hw6-dickinsj2.uc.r.appspot.com/oauth2callback'
+#for testing only
 #REDIRECT_URI = 'http://127.0.0.1:5000/oauth2callback'
 
+@app.route('/welcome')
+def welcome():
+    return app.send_static_file('welcome.html')
+
+#####################################################################################
+# new user flow:    
+#   redirect to /oauth2callback
+#   generate state and store in session
+#   redirect user to google with proper params (state included), user consents access
+#   get code back from google which redirects back to oauth2callback
+#   check that state is present and matches original state 
+#   send a post to googleapis.com/token from client with all credentials
+#   get back token, store in session, redirect back to index page
+#   ensure creds in place, use token to access user data from google
+#   format and display to user
+#####################################################################################
 
 @app.route('/')
 def index():
@@ -29,6 +50,8 @@ def index():
     r = requests.get(req_uri, headers=headers)
     res = json.loads(r.text)
     #return r.text
+    if 'state' not in flask.session:
+        return 'state not in session'
     userInfo = {'first name': res["names"][0]["givenName"],
                 'last name': res['names'][0]['familyName'],
                 'email': res['emailAddresses'][0]['value'],
@@ -39,26 +62,27 @@ def index():
 @app.route('/oauth2callback')
 def oauth2callback():
   if 'code' not in flask.request.args:
-    state = OA.generate_random_code(10)
+    state = rc.generate_random_code(10)
     auth_uri = ('https://accounts.google.com/o/oauth2/v2/auth?response_type=code'
                 '&client_id={}&redirect_uri={}&scope={}&state={}&prompt=consent').format(CLIENT_ID, REDIRECT_URI, SCOPE, state)
     flask.session['state'] = state
     return flask.redirect(auth_uri)
   else:
-    auth_code = flask.request.args.get('code')
-    data = {'code': auth_code,
-            'client_id': CLIENT_ID,
-            'client_secret': CLIENT_SECRET,
-            'redirect_uri': REDIRECT_URI,
-            'grant_type': 'authorization_code'}
-    r = requests.post('https://oauth2.googleapis.com/token', data=data)
-    flask.session['credentials'] = r.text
-    if 'state' in flask.session:
-        if flask.session['state'] != flask.request.args.get('state'):
-            return "States do not match"
-        return flask.redirect(flask.url_for('index'))
+    if 'state' not in flask.session:
+        return "State not in session"
     else:
-        return "state not in session"
+        if flask.session['state'] != flask.request.args.get('state'):
+            return "States do not match"  
+        auth_code = flask.request.args.get('code')
+        data = {'code': auth_code,
+                'client_id': CLIENT_ID,
+                'client_secret': CLIENT_SECRET,
+                'redirect_uri': REDIRECT_URI,
+                'grant_type': 'authorization_code'}
+        r = requests.post('https://oauth2.googleapis.com/token', data=data)
+        flask.session['credentials'] = r.text
+        return flask.redirect(flask.url_for('index'))
+
 
 if __name__ == '__main__':
   app.debug = False
