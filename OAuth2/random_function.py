afa26acfcb9da0254eb0771e274fca85f34b03ca
Jeff <dickinsj@oregonstate.edu>
2020-05-12 09:47:49 -0500

    random function comments

--- OAuth2/random_function.py
+++ OAuth2/random_function.py
@@ -2,6 +2,8 @@ import requests
 import random
 import string
 
+#input length parameter and get a string of random ascii letters and digits
+#used for app.secret_key and for 'state' variable in OAuth2 main.py
 def generate_random_code(stateLength=10):
     chars = string.ascii_letters + string.digits
     return ''.join((random.choice(chars) for i in range(stateLength)))
